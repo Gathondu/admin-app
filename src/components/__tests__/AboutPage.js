adf25da278347f80077ec3d7a293a27124597955
Denis Ngugi Gathondu <thundoss@gmail.com>
2019-01-02 23:56:57 +0300

    add snapshot tests for about page and author pages expect manageauthorpage
    Signed-off-by: Denis Ngugi Gathondu <thundoss@gmail.com>

---
+++ src/components/__tests__/AboutPage.js
@@ -0,0 +1,13 @@
+import renderer from 'react-test-renderer';
+import AboutPage from "../about/AboutPage";
+import React from "react";
+
+describe('The About Page Component', () => {
+    it('It does not regress',() => {
+        const title = 'About Test';
+        const rendered = renderer.create(
+            <AboutPage title={title} />
+        );
+        expect(rendered.toJSON()).toMatchSnapshot();
+    });
+});
---
+++ src/components/__tests__/Author.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {BrowserRouter} from 'react-router-dom';
+import AuthorForm from '../author/AuthorForm';
+import AuthorList from '../author/AuthorList';
+import AuthorListRow from '../author/AuthorListRow';
+import AuthorsPage from '../author/AuthorsPage';
+
+describe('Author Components',() => {
+    describe('AuthorForm Component', () => {
+        it('Does not regress', () => {
+            const props = {
+                loading: true,
+                onChange: ()=>{},
+                formIsValid: true,
+                onSave: ()=>{},
+                author: {
+                    firstName: 'first',
+                    lastName: 'last'
+                },
+                errors: {
+                    firstName: 'first error',
+                    lastName: 'last error'
+                }
+            };
+            const tree = renderer.create(
+                <AuthorForm loading={props.loading}
+                            onChange={props.onChange}
+                            formIsValid={props.formIsValid}
+                            onSave={props.onSave}
+                            author={props.author}
+                            errors={props.errors}/>
+            );
+            expect(tree.toJSON()).toMatchSnapshot();
+        });
+    });
+    describe('AuthorListRow Component',()=>{
+        it('does not regress',()=>{
+            const tree = renderer.create(
+                <BrowserRouter>
+                    <AuthorListRow author={{firstName: 'first', lastName:'last'}}
+                               deleteAuthor={()=>{}}/>
+                </BrowserRouter>
+            );
+            expect(tree.toJSON()).toMatchSnapshot();
+        });
+    });
+    describe('AuthorList Component',()=>{
+        it('should not regress',()=>{
+            const tree = renderer.create(
+                <AuthorList loading={true} deleteAuthor={()=>{}} authors={[]}/>
+            );
+            expect(tree.toJSON()).toMatchSnapshot();
+        });
+    });
+    describe('AuthorsPage Component',()=>{
+        it('should not regress',()=>{
+            const tree = renderer.create(
+                <AuthorsPage loading={false} authors={[]} deleteAuthor={()=>{}} title={'Test Title'}/>
+            );
+            expect(tree.toJSON()).toMatchSnapshot();
+        });
+    });
+});
